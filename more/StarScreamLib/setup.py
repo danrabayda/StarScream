ae4316729621e87484dfcd23a9902233831c1df0
Daniel Rabayda <48996871+danrabayda@users.noreply.github.com>
2022-07-12 14:49:58 -0400

    Add files via upload

---
+++ more/StarScreamLib/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='StarScreamLib',
+    packages=find_packages(include=['StarScreamLib']),
+    version='0.1.0',
+    description='A library for working with aircraft audios',
+    author='Daniel Rabayda',
+    license='MIT',
+)
